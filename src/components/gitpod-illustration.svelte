386fb44c3e3a1ffa95a5f5feae04dda33c85df40
Nisar Hassan Naqvi <syednisarhassan12@gmail.com>
2021-08-12 16:10:25 +0000

    create the unsubscribe page + fix some other issues.

---
+++ src/components/gitpod-illustration.svelte
@@ -0,0 +1,5 @@
+<img
+  class="mt-small md:mt-x-large w-full max-w-6xl bg-right-top mx-auto block"
+  src="/images/illustration-crop.jpg"
+  alt="Gitpod Illustration"
+/>
---
+++ src/routes/unsubscribe.svelte
@@ -0,0 +1,38 @@
+<script>
+  import OpenGraph from "../components/open-graph.svelte";
+  import GitpodIllustration from "../components/gitpod-illustration.svelte";
+</script>
+
+<style type="text/postcss">
+  header {
+    @apply max-w-full !important;
+  }
+
+  h1 {
+    @apply text-h3 !important;
+    @media (max-width: 1040px) {
+      @apply mt-medium;
+    }
+  }
+</style>
+
+<OpenGraph
+  data={{
+    title: "Unsubscribed",
+    description: "",
+    norobots: true,
+  }}
+/>
+
+<header class="card shadow-xl mb-32 text-center sm:mx-8">
+  <h1 class="h5">Unsubscribed</h1>
+  <p class="max-w-2xl text-large mx-auto">
+    You have been sucessfully unsubscribed. If you would like to revert it,
+    please go to <a
+      href="https://gitpod.io/notifications"
+      target="_blank"
+      rel="nofollow">gitpod.io/notifications</a
+    >.
+  </p>
+  <GitpodIllustration />
+</header>
